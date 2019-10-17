9427a90f3a71a497f429b69a779413756c3dce50
SwastikBhatt <47355542+SwastikBhatt@users.noreply.github.com>
2019-10-17 18:45:10 +0530

    Add files via upload

---
+++ CamundaStarter/src/main/java/com/camunda/bpmn/CamundaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.camunda.bpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaStarterApplication.class, args);
+	}
+
+}
